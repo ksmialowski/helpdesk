2074a5f5d9b5745f1f5d2e07674c47032f01924c
Kamil Śmiałowski <smialowski.kamil@gmail.com>
2025-11-16 23:06:12 +0100

    feat: add Role model, roles migration and user->role relationship

---
+++ backend/database/migrations/2025_11_16_213442_create_roles_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->timestamps();
+        });
+
+        foreach (['admin', 'agent', 'reporter'] as $role) {
+            DB::table('roles')->insert([
+                'name' => $role,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+    }
+};
